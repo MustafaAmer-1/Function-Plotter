c5a20bd8511e658a3970cf766767fb1fa1ff2938
Mustafa Amer <1806185@eng.asu.edu.eg>
2021-12-21 08:34:21 +0200

    Testing validate expression function

--- AutoTestPlotter/tst_testplotter.cpp
+++ AutoTestPlotter/tst_testplotter.cpp
@@ -6,7 +6,29 @@
 class TestPlotter : public QObject
 {
     Q_OBJECT
+
+private slots:
+    void validate();
+    void validate_data();
 };
+
+void TestPlotter::validate()
+{
+    QFETCH(QString, expression);
+    QFETCH(bool, validity);
+
+    QCOMPARE(Plotter::getPlotter(expression)->validate(), validity);
+}
+
+void TestPlotter::validate_data(){
+    QTest::addColumn<QString>("expression");
+    QTest::addColumn<bool>("validity");
+
+    QTest::newRow("valid exp") << "x^2+5x+35*12*x+70" << true;
+    QTest::newRow("statring wrong")     << "*x+5" << false;
+    QTest::newRow("ending wrong") << "x+5*" << false;
+    QTest::newRow("middle wrong") << "x^9+50x++95*2*x+702" << false;
+}
 
 QTEST_APPLESS_MAIN(TestPlotter)
 
